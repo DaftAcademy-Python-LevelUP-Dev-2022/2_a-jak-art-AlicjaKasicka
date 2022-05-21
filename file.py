788b4533fda3e15488d3471b441cd4d4e51934da
AlicjaKasicka <alice55451@gmail.com>
2022-05-21 19:47:02 +0200

    second task

--- file.py
+++ file.py
@@ -11,7 +11,27 @@ def greeter(func):
 
 
 def sums_of_str_elements_are_equal(func):
-    pass
+    def summer(num1, num2):
+        if sum(num1) == sum(num2):
+            return f'{sum(num1)} == {sum(num2)}'
+        else:
+            return f'{sum(num1)} != {sum(num2)}'
+
+    def splitter(*args):
+        numbers = func(*args).split()
+
+        if numbers[0][0] != '-':
+            num1 = [int(n) for n in numbers[0]]
+        else:
+            num1 = [-int(n) for n in numbers[0][1:]]
+        
+        if numbers[1][0] != '-':
+            num2 = [int(n) for n in numbers[1]]
+        else:
+            num2 = [-int(n) for n in numbers[1][1:]]
+        
+        return summer(num1, num2)
+    return splitter
 
 
 def format_output(*required_keys):
