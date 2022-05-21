44ce648d7b54d2590bb9c7b57a4f75cbc474dcca
AlicjaKasicka <alice55451@gmail.com>
2022-05-21 20:30:16 +0200

    third task

--- file.py
+++ file.py
@@ -35,7 +35,40 @@ def sums_of_str_elements_are_equal(func):
 
 
 def format_output(*required_keys):
-    pass
+    keys = []
+    for key in required_keys:
+        keys.append(key.split("__"))
+    
+    def real_decorator(func):
+        def checker(*args):
+            funct_keys = func(*args)
+            new_dict = {}
+            
+            for key in keys:
+                if len(key) == 1:
+                    if key[0] not in funct_keys.keys():
+                        raise ValueError
+                    else:
+                        if not funct_keys[key[0]]:
+                            new_dict[key[0]] = "Empty value"
+                        else:
+                            new_dict[key[0]] = funct_keys[key[0]]
+                else:
+                    new_str = ""
+                    for k in key:
+                        if k not in funct_keys.keys():
+                            raise ValueError
+                        else:
+                            new_str += " " + funct_keys[k]
+
+                    new_key = "__".join(key)
+                    if not new_str:
+                        new_dict[new_key] = "Empty value"
+                    else:
+                        new_dict[new_key] = new_str[1:]
+            return new_dict
+        return checker
+    return real_decorator
 
 
 def add_method_to_instance(klass):
